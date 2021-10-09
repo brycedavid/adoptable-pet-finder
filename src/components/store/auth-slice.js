7ea47280921c24a2ffc601631910731be2d66a8d
Bryce David Landry <brycedavid66@gmail.com>
2021-10-09 13:08:39 -0500

    beginnings of authentication Redux logic

---
+++ src/components/store/auth-slice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = { isAuthenticated: false };
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const authActions = authSlice.actions;
+
+export default authSlice;
---
+++ src/components/store/index.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./auth-slice";
+
+const store = configureStore({
+  reducer: { auth: authSlice.reducer },
+});
+
+export default store;
