bc64b6921351771200fe9617a1b17ad601ae4dff
Bryce David Landry <brycedavid66@gmail.com>
2021-10-09 13:41:17 -0500

    styling tweaks

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,6 +7,16 @@ import SearchForm from "../Search/SearchForm";
 const Home = () => {
   return (
     <div className={classes["main-content"]}>
+      <div className={classes["homepage-text-container"]}>
+        <h3>
+          Looking to adopt a furry friend? You've come to the right place!
+        </h3>
+        <p>
+          With over a hundred thousand pets ready for adoption from over ten
+          thousand organizations, there's no doubt we'll find the right member
+          to add to the family!
+        </p>
+      </div>
       <Card class="main-image-container">
         <Image
           class="main-image"
@@ -14,14 +24,7 @@ const Home = () => {
           source="https://www.petlink.net/wp-content/uploads/2019/04/Puppy-and-Kitten-Closeup-Over-White-649091176_2052x1466.jpeg"
         />
       </Card>
-      <div className={classes["homepage-text-container"]}>
-        <p>
-          Looking to adopt a furry friend? You've come to the right place! With
-          over a hundred thousand pets ready for adoption from over ten thousand
-          organizations, there's no doubt we'll find the right member to add to
-          the family!
-        </p>
-      </div>
+
       <Card class="main-search-container">
         <SearchForm />
       </Card>
