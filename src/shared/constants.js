9ecaf2d64bb8d6af82ca375dcf2770325103efc1
Bryce David Landry <brycedavid66@gmail.com>
2022-01-06 17:41:23 -0600

    Added URLs as constants

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,2 +1,9 @@
 export const apiKey = "YeI5i5zLHnqvUoBxfJcjseCpBDQcZSS6ecZKJouXs07aejuKfK";
 export const secret = "WhuKLwumWocRsjzuQYPVSC6ZybxuMdhVCRXYIIW6";
+
+// Page URLs
+
+export const homeUrl = "/";
+export const adoptionCentersUrl = "/adoption-centers";
+export const adoptablePetsUrl = "/adoptable-pets";
+export const aboutUrl = "/about";
