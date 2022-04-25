9660267aced2f6f37fc35977de65cc81ae88e6a0
Bryce David Landry <bryce.landry@gmfinancial.com>
2022-04-25 11:45:03 -0500

    implemented mobile navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,16 +2,90 @@
 // This is the navbar that is present on the top of every page.
 // It renders Subnavs as children, which allow the user to navigate to different pages.
 
+import React, { useState, useEffect } from "react";
 import Subnav from "./Subnav";
 
 const Navbar = () => {
+  const [isMobileViewport, setIsMobileViewport] = useState(false);
+
+  const ref = React.useRef();
+
+  // Track whether or not the viewport width is <= 460. If so, set isMobileViewport to true. If not, set isMobileViewport
+  // to false.
+  useEffect(() => {
+    if (window.innerWidth <= 550) {
+      setIsMobileViewport(true);
+    } else {
+      setIsMobileViewport(false);
+    }
+
+    const updateMedia = () => {
+      if (window.innerWidth <= 550) {
+        setIsMobileViewport(true);
+      } else {
+        setIsMobileViewport(false);
+      }
+    };
+
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, []);
+
+  if (isMobileViewport) {
+    return (
+      <div className="navbar--mobile">
+        <input
+          type="checkbox"
+          class="navbar--mobile__checkbox"
+          id="navi-toggle"
+          ref={ref}
+        />
+        <label for="navi-toggle" class="navbar--mobile__button">
+          <span className="navbar--mobile__icon">&nbsp;</span>
+        </label>
+        <div className="navbar--mobile__background">&nbsp;</div>
+        <div className="navbar--mobile__nav">
+          <ul>
+            <Subnav page="Home" to="/" mobileVersion={true} checkboxRef={ref} />
+            <Subnav
+              page="Adoption Centers"
+              to="/adoption-centers"
+              mobileVersion={true}
+              checkboxRef={ref}
+            />
+            <Subnav
+              page="Adoptable Pets"
+              to="/adoptable-pets"
+              mobileVersion={true}
+              checkboxRef={ref}
+            />
+            <Subnav
+              page="About"
+              to="/about"
+              mobileVersion={true}
+              checkboxRef={ref}
+            />
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <nav className="navbar">
       <ul>
-        <Subnav page="Home" to="/" />
-        <Subnav page="Adoption Centers" to="/adoption-centers" />
-        <Subnav page="Adoptable Pets" to="/adoptable-pets" />
-        <Subnav page="About" to="/about" />
+        <Subnav page="Home" to="/" mobileVersion={false} />
+        <Subnav
+          page="Adoption Centers"
+          to="/adoption-centers"
+          mobileVersion={false}
+        />
+        <Subnav
+          page="Adoptable Pets"
+          to="/adoptable-pets"
+          mobileVersion={false}
+        />
+        <Subnav page="About" to="/about" mobileVersion={false} />
       </ul>
     </nav>
   );
--- src/components/Navbar/Subnav.js
+++ src/components/Navbar/Subnav.js
@@ -4,12 +4,22 @@
 import { NavLink } from "react-router-dom";
 
 const Subnav = (props) => {
+  const mobileMenuCollapseHandler = () => {
+    console.log(props.checkboxRef);
+    props.checkboxRef.current.checked = !props.checkboxRef.current.checked;
+  };
+
   return (
-    <li className="subnav">
+    <li
+      className={props.mobileVersion ? "subnav--mobile" : "subnav"}
+      onClick={props.mobileVersion ? mobileMenuCollapseHandler : null}
+    >
       <NavLink
         exact
-        activeClassName="active"
-        className="subnav__link"
+        activeClassName=""
+        className={
+          props.mobileVersion ? "subnav--mobile__link" : "subnav__link"
+        }
         to={props.to}
       >
         {props.page}
